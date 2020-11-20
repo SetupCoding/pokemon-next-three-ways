8fae91e62c98e3614d821ac82a1c1f36f249d81a
Anton Schmidt <anton.schmidt@mhp.com>
2020-11-20 19:14:35 +0100

    perf: improve performance of apis for empty requests feat: fetch pokemon by id instead of english name

--- csr/pages/api/pokemon.ts
+++ csr/pages/api/pokemon.ts
@@ -3,19 +3,20 @@ import { NextApiRequest, NextApiResponse } from "next";
 import pokemon from "../../pokemon.json";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
+  const idToFind = req.query.id as string;
+
+  if (!idToFind) {
     res.statusCode = 400;
     res.end("Must have a name");
   } else {
-    const found = pokemon.filter(
-      ({ name: { english } }) => english === req.query.name
-    );
-    if (found.length === 0) {
+    const found = pokemon.find(({ id }) => id === +idToFind);
+    if (!found) {
       res.statusCode = 404;
-      res.end(`Pokemon ${req.query.name} not found`);
+      res.end(`Pokemon with id ${req.query.id} not found`);
     } else {
       res.statusCode = 200;
       res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify(found[0]));
+      res.end(JSON.stringify(found));
     }
   }
 };
