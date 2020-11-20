252ebd6b953c7b21878e4cb97355ee61bf89fcb7
Anton Schmidt <anton.schmidt@mhp.com>
2020-11-20 20:56:57 +0100

    feat: add error and loading component

---
+++ csr/components/Loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Row } from "react-bootstrap";
+type Props = {
+  isLoading: boolean;
+};
+export const Loading: React.FC<Props> = ({ isLoading }) =>
+  isLoading ? (
+    <Row className="justify-content-md-center">Loading...</Row>
+  ) : null;
---
+++ csr/components/index.ts
@@ -0,0 +1,2 @@
+export {Loading} from './Loading'
+export {ErrorComponent} from './ErrorComponent'
