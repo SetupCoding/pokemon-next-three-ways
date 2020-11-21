2cc7363a46db70bef050c399fda56bf12812cbb1
Anton Schmidt <anton.schmidt@mhp.com>
2020-11-21 22:37:27 +0100

    feat: make loading parameter optional

--- csr/components/Loading.tsx
+++ csr/components/Loading.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Row } from "react-bootstrap";
 type Props = {
-  isLoading: boolean;
+  isLoading?: boolean;
 };
 export const Loading: React.FC<Props> = ({ isLoading }) =>
   isLoading ? (
