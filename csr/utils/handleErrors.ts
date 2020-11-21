e170b10dd98db1f50114ab6a55f7988880ff7b32
Anton Schmidt <anton.schmidt@mhp.com>
2020-11-21 20:47:29 +0100

    style: format document

--- csr/utils/handleErrors.ts
+++ csr/utils/handleErrors.ts
@@ -1,7 +1,7 @@
-export const handleErrors=async (res)=>  {
-    if (res.status >= 200 && res.status <= 299) {
-        return (await res.json())
-      } else {
-        throw Error(res.statusText);
-      }
-}
+export const handleErrors = async (res) => {
+  if (res.status >= 200 && res.status <= 299) {
+    return await res.json();
+  } else {
+    throw Error(res.statusText);
+  }
+};
