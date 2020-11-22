e92f5c367127f2895303aa2e2c838ce65180fc4b
Anton Schmidt <anton.schmidt@mhp.com>
2020-11-22 17:09:51 +0100

    tesst: add PokemonCard tests

---
+++ ssr/tests/PokemonCard.test.tsx
@@ -0,0 +1,30 @@
+import { PokemonCard } from "../components";
+import { PokemonType } from "../models/pokemon.model";
+import React from "react";
+import { testSnapshot } from "../utils/testUtils";
+
+describe("<PokemonCard />", () => {
+  const props = {
+    id: 100,
+    name: {
+      english: "Voltorb",
+      japanese: "ビリリダマ",
+      chinese: "霹雳电球",
+      french: "Voltorbe",
+    },
+    type: ["Electric"] as PokemonType[],
+    image: "/pokemon/voltorb.png",
+    pokemonCount: 889,
+    base: {
+      HP: 40,
+      Attack: 30,
+      Defense: 50,
+      "Sp.Attack": 55,
+      "Sp.Defense": 55,
+      Speed: 100,
+    },
+  };
+  it("renders the PokemonCard component correctly", () => {
+    testSnapshot(<PokemonCard {...props} />);
+  });
+});
