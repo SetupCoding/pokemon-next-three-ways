5ba5f50c556216e3cf548ca912334efaf7034da8
Anton Schmidt <anton.schmidt@mhp.com>
2020-12-17 15:00:20 +0100

    feat: add missigno to the public dir where it was ironically missing

--- ssg/pages/404.tsx
+++ ssg/pages/404.tsx
@@ -1,6 +1,5 @@
 import { Container, Row } from "react-bootstrap";
 
-import Image from "next/image";
 import Link from "next/link";
 
 export const Custom404: React.FC = () => (
@@ -13,16 +12,18 @@ export const Custom404: React.FC = () => (
         <Row className="justify-content-md-center">
           <h1>Missingno will lead you back home</h1>
         </Row>
-        <Row className="justify-content-md-center">
-          <Image
-            src={"/404Pokemon.png"}
-            width={201}
-            height={225}
-            alt="404 Pokemon"
-          />
-        </Row>
       </a>
     </Link>
+    <Row className="justify-content-md-center">
+      <img
+        src={`/404Pokemon.png`}
+        alt="404 Pokemon"
+        style={{
+          width: "201px",
+          height: "225px",
+        }}
+      />
+    </Row>
   </Container>
 );
 
